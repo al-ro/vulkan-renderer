bf180626c16d4009fe71abb319c9a0a5863836d6
al-ro <>
2023-10-21 13:15:40 +0300

    Image viewes

--- main.cpp
+++ main.cpp
@@ -93,6 +93,7 @@ class HelloTriangleApplication {
   VkSwapchainKHR swapChain;
   // Contained images are created and destroyed automatically
   std::vector<VkImage> swapChainImages;
+  std::vector<VkImageView> swapChainImageViews;
   VkFormat swapChainImageFormat;
   VkExtent2D swapChainExtent;
 
@@ -357,6 +358,32 @@ class HelloTriangleApplication {
     swapChainExtent = extent;
   }
 
+  /*----- Image views -----*/
+  void createImageViews() {
+    swapChainImageViews.resize(swapChainImages.size());
+    for (size_t i = 0; i < swapChainImages.size(); i++) {
+      VkImageViewCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      // Note swapChainImages
+      createInfo.image = swapChainImages[i];
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = swapChainImageFormat;
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+      // Note swapChainImageViews
+      if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create image views!");
+      }
+    }
+  }
+
   /*----- Device -----*/
 
   bool checkSwapChainSupport(const VkPhysicalDevice& device) {
@@ -471,6 +498,7 @@ class HelloTriangleApplication {
     createLogicalDevice();
     // Must be called after logical device creation
     createSwapChain();
+    createImageViews();
   }
 
   /*----- Debgug Messenger -----*/
@@ -524,6 +552,9 @@ class HelloTriangleApplication {
   void cleanup() noexcept {
     if (enableValidationLayers) {
       DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
+    for (auto imageView : swapChainImageViews) {
+      vkDestroyImageView(device, imageView, nullptr);
     }
     // Called before device destruction
     vkDestroySwapchainKHR(device, swapChain, nullptr);
