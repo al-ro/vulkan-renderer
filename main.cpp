300917838f497659828e9eed78a978b9c440ae64
al-ro <>
2023-10-27 18:06:38 +0300

    Framebuffers

--- main.cpp
+++ main.cpp
@@ -125,6 +125,8 @@ class HelloTriangleApplication {
   VkRenderPass renderPass;
   VkPipeline graphicsPipeline;
 
+  std::vector<VkFramebuffer> swapChainFramebuffers;
+
   /*----- GLFW  -----*/
 
   void initWindow() {
@@ -434,6 +436,29 @@ class HelloTriangleApplication {
     }
   }
 
+  /*----- Framebuffer -----*/
+
+  void createFramebuffers() {
+    swapChainFramebuffers.resize(swapChainImageViews.size());
+
+    for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+      VkImageView attachments[] = {swapChainImageViews[i]};
+
+      VkFramebufferCreateInfo framebufferInfo{};
+      framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+      framebufferInfo.renderPass = renderPass;
+      framebufferInfo.attachmentCount = 1;
+      framebufferInfo.pAttachments = attachments;
+      framebufferInfo.width = swapChainExtent.width;
+      framebufferInfo.height = swapChainExtent.height;
+      framebufferInfo.layers = 1;
+
+      if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create framebuffer");
+      }
+    }
+  }
+
   /*----- Queue -----*/
 
   QueueFamilyIndices findQueueFamilies(const VkPhysicalDevice& device) {
@@ -736,6 +761,7 @@ class HelloTriangleApplication {
     createImageViews();
     createRenderPass();
     createGraphicsPipeline();
+    createFramebuffers();
   }
 
   /*----- Debgug Messenger -----*/
@@ -792,6 +818,9 @@ class HelloTriangleApplication {
     }
     for (auto imageView : swapChainImageViews) {
       vkDestroyImageView(device, imageView, nullptr);
+    }
+    for (auto framebuffer : swapChainFramebuffers) {
+      vkDestroyFramebuffer(device, framebuffer, nullptr);
     }
     // Called before device destruction
     vkDestroySwapchainKHR(device, swapChain, nullptr);
